659a88031cdf3ee3720fbc7781c368ca38b3bd39
Nicolas QUINQUENEL <nicolas.quinquenel@etu.univ-nantes.fr>
2016-10-23 13:47:19 +0200

    Update Satellite.py // Ajout commentaires

--- PolyHash_tmp/Satellite.py
+++ PolyHash_tmp/Satellite.py
@@ -24,22 +24,24 @@ class Satellite:
         return True
     
     def calculePosition(self):
-        print("Latitude : " , self.latitude , "; Longitude : " , self.longitude , "; Vitesse : " , self.vitesse)
+        #Si latitude + vitesse se trouve entre -90° et 90°
         if self.latitude <= 324000 and self.latitude >= -324000:
             self.latitude = self.latitude + self.vitesse
             self.longitude = self.longitude - 15
+        #Si latitude + vitesse se trouve superieur à 90° (il vient de depasser le Pole Nord)
         elif self.latitude > 324000:
             self.latitude = (324000*2) - (self.latitude + self.vitesse)
             self.longitude = -(324000*2) + (self.longitude - 15)
             self.vitesse = -self.vitesse
+        #Si latitude + vitesse se trouve infenieur à 90° (il vient de depasser le Pole Sud)
         else:
             self.latitude = -(324000*2) - (self.latitude + self.vitesse)
             self.longitude = -(324000*2) + (self.longitude - 15)
             self.vitesse = -self.vitesse
+        #Si longitude depasse -648000" alors longitude repasse à 647999"
         if self.longitude < -648000:
             self.longitude = 648000 - (-self.longitude - 648000)
         print("Latitude : " , self.latitude , "; Longitude : " , self.longitude , "; Vitesse : " , self.vitesse)
-        return 0
     
     def getOrientationSatellite(self):
         return self.deltaLatitude, self.deltaLongitude
