71bfe667185bf9ba139535b73ebb170119e8d159
Arthur Outhier <arthur.outhier@etu.univ-nantes.fr>
2016-10-28 16:37:06 +0200

    modification de la classe Collection

--- PolyHash_tmp/Collection.py
+++ PolyHash_tmp/Collection.py
@@ -4,38 +4,37 @@ Created on 27 oct. 2016
 @authors: Mathieu Soyer, Nicolas Ehresmann
 '''
 
-from Satellite import Satellite
-
 class Collection:
 
-    def __init__(self,valeurPoints,listeCoordonnees,tempsRequis):
-	self.valeurPoints=valeurPoints
-	self.listeCoordonnees=listeCoordonnees
-	self.listeCoordoneesReussies=[]
-	self.tempsRequis=tempsRequis
-    @classmethod
-    def valeurs(cls,valeurs):                                  //2eme constructeur
-	return cls(valeurs,listeCoordonnees=[],tempsRequis=[])	
-    
+    def __init__(self,valeurPoints,listeCoordonnees=None,tempsRequis=None):
+        self.listeCoordoneesReussies= []
+        self.valeurPoints = valeurPoints
+        if(tempsRequis != None or listeCoordonnees != None):
+            self.listeCoordonnees = listeCoordonnees
+            self.tempsRequis = tempsRequis
+        else:
+            self.listeCoordonnes = []
+            self.tempsRequis = []
 
     def getvaleur():
-	return self.valeurPoints
+        return self.valeurPoints
 	
     def getCoordonnees():
-	return self.listeCoordonnees
+        return self.listeCoordonnees
+
+    def getTemps():
+        return self.tempsRequis
+    
     def getCoordonneesReussies():
-	return self.getCoordonneesReussies
+        return self.getCoordonneesReussies
 			
-    def suppresionElement(liste):
-	if estVide():
-		return False
-	else:
-		self.listeCoordonnees.remove(liste)
-		return True
+    def suppressionElement(liste):
+            self.listeCoordonnees.remove(liste)
+            return estVide()
 
     def estVide():
-	if not self.listeCoordonnees:
-		return True
-	else:
-		return False
+        if not self.listeCoordonnees:
+            return True
+        else:
+            return False
    
