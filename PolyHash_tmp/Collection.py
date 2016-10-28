ea48b8ea8f852d455d76d8abfc718cff926a2b9e
nicolas <nicolas.ehresmann@univ-nantes.fr>
2016-10-28 15:07:49 +0200

    Code de la classe Collection

--- PolyHash_tmp/Collection.py
+++ PolyHash_tmp/Collection.py
@@ -1,11 +1,41 @@
 '''
 Created on 27 oct. 2016
 
-@author: Mathieu Soyer
+@authors: Mathieu Soyer, Nicolas Ehresmann
 '''
 
 from Satellite import Satellite
 
 class Collection:
 
-    def __init__(self):
+    def __init__(self,valeurPoints,listeCoordonnees,tempsRequis):
+	self.valeurPoints=valeurPoints
+	self.listeCoordonnees=listeCoordonnees
+	self.listeCoordoneesReussies=[]
+	self.tempsRequis=tempsRequis
+    @classmethod
+    def valeurs(cls,valeurs):                                  //2eme constructeur
+	return cls(valeurs,listeCoordonnees=[],tempsRequis=[])	
+    
+
+    def getvaleur():
+	return self.valeurPoints
+	
+    def getCoordonnees():
+	return self.listeCoordonnees
+    def getCoordonneesReussies():
+	return self.getCoordonneesReussies
+			
+    def suppresionElement(liste):
+	if estVide():
+		return False
+	else:
+		self.listeCoordonnees.remove(liste)
+		return True
+
+    def estVide():
+	if not self.listeCoordonnees:
+		return True
+	else:
+		return False
+   
